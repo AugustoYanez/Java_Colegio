aee3f906c426256078688725d7bbede82a8d6928
Augusto <101185871+AugustoYanez@users.noreply.github.com>
2023-08-23 09:53:52 -0300

    Add files via upload

---
+++ src/P/A/S/O/Candidato.java
@@ -0,0 +1,36 @@
+package P.A.S.O;
+
+import Fecha.Fecha;
+import Personas.Persona;
+
+import java.util.HashMap;
+
+public class Candidato extends Persona {
+
+private Partido_Politico partidoPolitico;
+
+private HashMap<Provincia, Integer> cantidadVotosPorProvincia;
+
+
+    public Partido_Politico getPartidoPolitico() {
+        return partidoPolitico;
+    }
+
+    public void setPartidoPolitico(Partido_Politico partidoPolitico) {
+        this.partidoPolitico = partidoPolitico;
+    }
+
+    public HashMap<Provincia, Integer> getCantidadVotosPorProvincia() {
+        return cantidadVotosPorProvincia;
+    }
+
+    public void setCantidadVotosPorProvincia(HashMap<Provincia, Integer> cantidadVotosPorProvincia) {
+        this.cantidadVotosPorProvincia = cantidadVotosPorProvincia;
+    }
+
+    public Candidato (String nombre, String apellido, int dni, Fecha nacimiento, Partido_Politico partidoPolitico, HashMap<Provincia, Integer> cantidadVotosPorProvincia){
+        super(nombre,apellido,dni,nacimiento);
+        this.partidoPolitico = partidoPolitico;
+        this.cantidadVotosPorProvincia = cantidadVotosPorProvincia;
+    }
+}
